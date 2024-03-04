ae650e8f787ce9d1b34167fbcf3bde050cfcfcaf
Oguzz <oguzaslan_203@hotmail.com>
2024-03-04 03:39:39 +0300

    CrossOrigin annotation added ("http://localhost:4200")

--- spring-boot-ecommerce/src/main/java/com/example/ecommerce/repository/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/example/ecommerce/repository/ProductRepository.java
@@ -2,6 +2,9 @@ package com.example.ecommerce.repository;
 
 import com.example.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
