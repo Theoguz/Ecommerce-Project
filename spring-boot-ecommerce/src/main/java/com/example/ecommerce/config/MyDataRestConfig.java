83a2a663e87477618b78b683c1a07f301b68d70a
Oguzz <oguzaslan_203@hotmail.com>
2024-03-10 02:40:33 +0300

    Added disableHttpMethods(Country.class-State.class)

--- spring-boot-ecommerce/src/main/java/com/example/ecommerce/config/MyDataRestConfig.java
+++ spring-boot-ecommerce/src/main/java/com/example/ecommerce/config/MyDataRestConfig.java
@@ -1,7 +1,9 @@
 package com.example.ecommerce.config;
 
+import com.example.ecommerce.entity.Country;
 import com.example.ecommerce.entity.Product;
 import com.example.ecommerce.entity.ProductCategory;
+import com.example.ecommerce.entity.State;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.EntityType;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +20,6 @@ import java.util.Set;
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
     private EntityManager entityManager;
-
     @Autowired
     public MyDataRestConfig(EntityManager entityManager) {
         this.entityManager = entityManager;
@@ -33,6 +34,8 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         // disable HTTP methods for ALL: PUT, POST and DELETE
         disableHttpMethods(Product.class, config, theUnsupportedActions);
         disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
+        disableHttpMethods(Country.class, config, theUnsupportedActions);
+        disableHttpMethods(State.class, config, theUnsupportedActions);
 
         // call an internal helper method
         exposeIds(config);
@@ -47,7 +50,6 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
     private void exposeIds(RepositoryRestConfiguration config) {
         // expose entity ids
-        //
         // - get a list of all entity classes from the entity manager
         Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
 
@@ -62,6 +64,5 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         config.exposeIdsFor(domainTypes);
 
     }
-
 
 }
