b00a69b7af65606e3b1db67ab51be7a8d5fe092b
Oguzz <oguzaslan_203@hotmail.com>
2024-03-13 06:02:34 +0300

    CrossOrigin removed cause of cors added

--- spring-boot-ecommerce/src/main/java/com/example/ecommerce/repository/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/example/ecommerce/repository/ProductRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("http://localhost:4200")
+//@CrossOrigin("http://localhost:4200")  removed to allow for global CORS configuration
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
--- spring-boot-ecommerce/src/main/java/com/example/ecommerce/repository/StateRepository.java
+++ spring-boot-ecommerce/src/main/java/com/example/ecommerce/repository/StateRepository.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
-@CrossOrigin("http://localhost:4200")
+//@CrossOrigin("http://localhost:4200") removed to allow for global CORS configuration
 @RepositoryRestResource
 public interface StateRepository extends JpaRepository<State, Integer> {
     List<State> findByCountryCode(@Param("code") String code);
