2c582735745da1731024b91f49fded9f6fdf5cc6
Oguzz <oguzaslan_203@hotmail.com>
2024-03-13 06:07:24 +0300

    added @Value annotation with allowed.origins

--- spring-boot-ecommerce/src/main/java/com/example/ecommerce/config/MyDataRestConfig.java
+++ spring-boot-ecommerce/src/main/java/com/example/ecommerce/config/MyDataRestConfig.java
@@ -7,6 +7,7 @@ import com.example.ecommerce.entity.State;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.EntityType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -19,7 +20,12 @@ import java.util.Set;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Value("${allowed.origins}")
+    private String[] theAllowedOrigins;
+
     private EntityManager entityManager;
+
     @Autowired
     public MyDataRestConfig(EntityManager entityManager) {
         this.entityManager = entityManager;
@@ -41,9 +47,9 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         // call an internal helper method
         exposeIds(config);
 
-        //configure cors mapping
 
-        cors.addMapping("/api/**").allowedOrigins("http://localhost:4200");
+        //configure cors mapping
+        cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);
     }
 
     private static void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
